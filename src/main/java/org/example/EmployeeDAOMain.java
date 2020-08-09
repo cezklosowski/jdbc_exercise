a9907bf01aece1362554053f439c9d5ffd6af2b5
cezklosowski <cez.klosowski@gmail.com>
2020-08-09 15:28:19 +0200

    console menu added

--- src/main/java/org/example/EmployeeDAOMain.java
+++ src/main/java/org/example/EmployeeDAOMain.java
@@ -25,36 +25,80 @@ public class EmployeeDAOMain {
 
         EmployeeDAO employeeDAO = new EmployeeDAO();
 
+        Scanner input = new Scanner(System.in);
+        boolean mainLoop = true;
+        String choice;
 
-        // create Employee
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Creating new employee");
-        System.out.println("Input id");
-        int id = Integer.parseInt(scanner.nextLine());
-        System.out.println("Input first name");
-        String firstName = scanner.nextLine();
-        System.out.println("Input last name");
-        String lastName = scanner.nextLine();
-        System.out.println("Input salary");
-        float salary = Float.parseFloat(scanner.nextLine());
-        System.out.println("Input date of employment (YYYY,MM,DD)");
-        String tempDate = scanner.nextLine();
-        String[] splitDate = tempDate.split(",");
-        LocalDate employmentDate = LocalDate.of(Integer.parseInt(splitDate[0]),Integer.parseInt(splitDate[1]),Integer.parseInt(splitDate[2]));
-        System.out.println("Input work position");
-        String workPosition = scanner.nextLine();
+        do {
 
-        Employee employee = new Employee(id,firstName,lastName,salary,employmentDate,workPosition);
-        employeeDAO.create(connection,employee);
+            System.out.println("Choose operation:");
+            System.out.println("1. Create new employee");
+            System.out.println("2. Read employee from database");
+            System.out.println("3. Update employee info");
+            System.out.println("4. Delete employee from database");
+            System.out.println("q. Exit");
 
-        // read Employee
-        System.out.println("Input id:");
-        int searchedId = Integer.parseInt(scanner.nextLine());
-        System.out.println(employeeDAO.read(connection, searchedId));
+            choice = input.nextLine();
 
-        // update Employee
-        // delete Employee
 
+
+
+            switch (choice) {
+
+                // create Employee
+                case ("1"):
+                    Scanner scanner = new Scanner(System.in);
+                    System.out.println("Creating new employee");
+                    System.out.println("Input id");
+                    int id = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Input first name");
+                    String firstName = scanner.nextLine();
+                    System.out.println("Input last name");
+                    String lastName = scanner.nextLine();
+                    System.out.println("Input salary");
+                    float salary = Float.parseFloat(scanner.nextLine());
+                    System.out.println("Input date of employment (YYYY,MM,DD)");
+                    String tempDate = scanner.nextLine();
+                    String[] splitDate = tempDate.split(",");
+                    LocalDate employmentDate = LocalDate.of(Integer.parseInt(splitDate[0]), Integer.parseInt(splitDate[1]), Integer.parseInt(splitDate[2]));
+                    System.out.println("Input work position");
+                    String workPosition = scanner.nextLine();
+
+                    Employee employee = new Employee(id, firstName, lastName, salary, employmentDate, workPosition);
+                    employeeDAO.create(connection, employee);
+
+                    break;
+
+                // read Employee
+                case ("2"):
+                    Scanner scanner2 = new Scanner(System.in);
+                    System.out.println("Input id:");
+                    int idToRead = Integer.parseInt(scanner2.nextLine());
+                    System.out.println(employeeDAO.read(connection, idToRead));
+                    break;
+
+                // update Employee
+                case ("3"):
+                    Scanner scanner3 = new Scanner(System.in);
+                    System.out.println("Input id");
+                    int idToUpdate = Integer.parseInt(scanner3.nextLine());
+                    employeeDAO.update(connection, idToUpdate);
+                    break;
+
+                case ("4"):
+                    // delete Employee
+                    Scanner scanner4 = new Scanner(System.in);
+                    System.out.println("Input id");
+                    int idToDelete = Integer.parseInt(scanner4.nextLine());
+                    employeeDAO.delete(connection, idToDelete);
+                    break;
+                case("q"):
+                    // exit
+                    mainLoop = false;
+                    break;
+            }
+
+        } while (!choice.equals("q"));
         connection.close();
     }
 }
