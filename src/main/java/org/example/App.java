5b9292e31df24eec4a4bfea0ff142c31cf31abfa
cezklosowski <cez.klosowski@gmail.com>
2020-08-09 12:48:46 +0200

    update salary

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,25 +7,56 @@ public class App {
     private static final String USER = "jdbc_company_user";
     private static final String PASSWORD = "password1234";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
 
+        } catch (SQLException ex) {
+            System.err.println("Error code: " + ex.getErrorCode());
+            ex.printStackTrace();
+        }
+        printTable(connection);
+        updateSalary(connection, 8, 500);
+
+        connection.close();
+
+
+    }
+
+    private static void printTable(Connection connection) {
         //AutoCloseable
         try (
-                Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
                 Statement statement = connection.createStatement();
         ) {
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employee");
             while (resultSet.next()) {
-                String id = resultSet.getString(1);
+                int id = resultSet.getInt(1);
                 String firstName = resultSet.getString(2);
                 String lastName = resultSet.getString(3);
-                String salary = resultSet.getString(4);
-                String employmentDate = resultSet.getString(5);
+                float salary = resultSet.getFloat(4);
+                Date employmentDate = resultSet.getDate(5);
                 String workPosition = resultSet.getString(6);
                 System.out.println("ID: " + id + " | First name: " + firstName + " | Last name: " + lastName + " | Salary: " + salary + " | Date of employment: " + employmentDate + " | Work position: " + workPosition);
+
             }
-        } catch (
-                SQLException ex) {
+        } catch (SQLException ex) {
+            System.err.println("Error code: " + ex.getErrorCode());
+            ex.printStackTrace();
+        }
+    }
+
+    private static void updateSalary(Connection connection, int monthWorked, float extraAmount) {
+        //AutoCloseable
+        try (
+                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE employee SET salary = salary + ? WHERE (SELECT TIMESTAMPDIFF(MONTH, employment_date, curdate())) > ?");
+        ) {
+            preparedStatement.setFloat(1, extraAmount);
+            preparedStatement.setInt(2, monthWorked);
+            int updatedRecords = preparedStatement.executeUpdate();
+            System.out.println("Updated " + updatedRecords + " records.");
+
+        } catch (SQLException ex) {
             System.err.println("Error code: " + ex.getErrorCode());
             ex.printStackTrace();
         }
